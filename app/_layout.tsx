2983cd3a7288bf3b4b44e7cc28bccd904ac0ba6a
Tediisu <siegfredmoneva@gmail.com>
2025-09-05 14:56:27 +0800

    Initial Commit

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import './globals.css';
+
+export default function RootLayout() {
+  return <Stack>
+    <Stack.Screen 
+      name="index"
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen 
+      name="signUp"
+      options={{ headerShown: false }}
+    />
+  </Stack>;
+}
